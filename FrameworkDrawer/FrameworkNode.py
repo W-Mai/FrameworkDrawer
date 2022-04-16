c394704a746c60fa1a003d177ef567d67c7b9175
W-Mai <1341398182@qq.com>
2022-04-17 02:43:32 +0800

    特性：增加model to dict

--- FrameworkDrawer/FrameworkNode.py
+++ FrameworkDrawer/FrameworkNode.py
@@ -9,14 +9,34 @@ import sys
 from Signals import *
 
 
+# {
+#       "name":    "ModelName",
+#       "signals": [],
+# }
+#
+
 class ModelBoxBaseModelMetaclass(type):
     def __new__(mcs, name, bases, attrs):
-        mcs.attrs = attrs
+        mcs._attrs = attrs
         return type.__new__(mcs, name, bases, attrs)
+
+    @property
+    def attrs(self):
+        return self._attrs
+
+    @property
+    def meta_dict(self):
+        meta_class = ModelBoxBaseModel.attrs.get('Meta', None)
+        if meta_class is not None:
+            return {
+                'name': getattr(meta_class, 'name', None),
+            }
 
 
 class ModelBoxBaseModel(object, metaclass=ModelBoxBaseModelMetaclass):
     def __init__(self):
+        self._signals = []
+
         for attr in ModelBoxBaseModel.attrs.items():
             sig_name: str = attr[0]
             sig_obj = attr[1]
@@ -24,11 +44,19 @@ class ModelBoxBaseModel(object, metaclass=ModelBoxBaseModelMetaclass):
                 # sig_obj: SignalBase
                 if sig_obj.alias is None:
                     setattr(sig_obj, 'alias', sig_name)
+                self._signals.append(sig_obj)
 
-                print(sig_name, sig_obj.alias, sig_obj.label)
+    def to_dict(self):
+        meta_class = ModelBoxBaseModel.meta_dict
+
+        return {
+            "name": meta_class.get('name', self.__class__.__name__),
+            "signals": [sig.to_dict() for sig in self._signals]
+        }
 
 
 if __name__ == '__main__':
+
     class TestModel(ModelBoxBaseModel):
         CLK1 = Wire("CLK")
         RST = Wire("RST")
@@ -36,15 +64,14 @@ if __name__ == '__main__':
         Q = Reg("Q", (0, 31))
 
         class Meta:
-            label = "TestModel"
+            name = "TestMode1l"
 
-    a = (__import__('FrameworkNode'))
+
+    # a = (__import__('FrameworkNode'))
     import inspect
 
-    for name, obj in inspect.getmembers(sys.modules[__name__]):
-        if inspect.isclass(obj):
-            print(name, obj)
-
-    print(a)
-
-    m = TestModel()
+    for name, model in filter(
+            lambda x: inspect.isclass(x[1]) and issubclass(x[1], ModelBoxBaseModel) and not x[1] == ModelBoxBaseModel,
+            inspect.getmembers(sys.modules[__name__])):
+        # print(name, model)
+        print(model().to_dict())
