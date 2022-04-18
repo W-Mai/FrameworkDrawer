4856c4d34bd6be70adabb2bc54aadfef8bab7e28
W-Mai <1341398182@qq.com>
2022-04-19 00:25:43 +0800

    测试：编写新的示例

--- FrameworkDrawer/Tests/MyNode.py
+++ FrameworkDrawer/Tests/MyNode.py
@@ -5,7 +5,7 @@ class TestModel(ModelBoxBaseModel):
     CLK1 = Wire("CLK")
     RST = Wire("RST")
     D = Wire("D", (0, 31))
-    Q = Reg("Q", (0, 31))
+    Q = Reg("666", (0, 31))
 
     class Meta:
         name = "TestMode1l"
@@ -15,7 +15,7 @@ class TestModel2(ModelBoxBaseModel):
     CLK1 = Wire("CLK")
     RST = Wire("RST")
     D = Wire("D", (0, 31))
-    Q2 = Reg("QQQ", (0, 31))
+    Q2 = Reg("666", (0, 31))
 
     class Meta:
         name = "TestMode2"
@@ -24,7 +24,7 @@ class TestModel2(ModelBoxBaseModel):
 CONFIG = CONFIGURE(
     node_pos_pair={
         TestModel: (0, 10),
-        TestModel2: (0, 20),
+        TestModel2: (10, 20),
     }, other_conf={
         TestModel: {
             "flag": True,
--- FrameworkDrawer/Tests/node_test.py
+++ FrameworkDrawer/Tests/node_test.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import unittest
 import inspect
@@ -12,8 +13,19 @@ if __name__ == '__main__':
         raise Exception('CONFIGURE class not found.')
     configure: CONFIGURE = configures[0][1]
     print(configure.COLORS)
+    framework_draw_file_dict = {
+        'drawing_info': {},
+        'models': [],
+        'colors': configure.COLORS,
+    }
+
     for name, model in filter(
             lambda x: inspect.isclass(x[1]) and issubclass(x[1], ModelBoxBaseModel) and not x[1] == ModelBoxBaseModel,
             inspect.getmembers(a)):
-        # print(model().to_dict())
-        print(model().to_dict())
+        model_ins = model()
+        framework_draw_file_dict['models'].append(model_ins.to_dict())
+        framework_draw_file_dict['drawing_info'][name] = model_ins.other_conf
+
+    print(json.dumps(framework_draw_file_dict, default=lambda o: o.export))
+    json.dump(framework_draw_file_dict, open('../../model.json', 'w'), default=lambda o: o.export)
+    # print(framework_draw_file_dict)
