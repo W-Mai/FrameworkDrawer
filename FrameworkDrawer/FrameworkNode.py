d46eaf36fc61a221bc8eeb40fd0bd127a4b79fea
W-Mai <1341398182@qq.com>
2022-04-18 22:26:24 +0800

    特性：支持自定义conf、新的配置方式

--- FrameworkDrawer/FrameworkNode.py
+++ FrameworkDrawer/FrameworkNode.py
@@ -4,9 +4,10 @@
 # Mail: 1341398182@qq.com
 # Created Time:  2022-04-16
 #############################################
-import sys
+from optparse import Option
+from typing import Dict, Type, Optional
 
-from Signals import *
+from .Signals import *
 
 
 # {
@@ -17,27 +18,22 @@ from Signals import *
 
 class ModelBoxBaseModelMetaclass(type):
     def __new__(mcs, name, bases, attrs):
-        mcs._attrs = attrs
-        return type.__new__(mcs, name, bases, attrs)
+        mcs._attrs = attrs.copy()
+        attrs["_attrs"] = attrs
+        return super().__new__(mcs, name, bases, attrs)
 
     @property
     def attrs(self):
         return self._attrs
 
-    @property
-    def meta_dict(self):
-        meta_class = ModelBoxBaseModel.attrs.get('Meta', None)
-        if meta_class is not None:
-            return {
-                'name': getattr(meta_class, 'name', None),
-            }
-
 
 class ModelBoxBaseModel(object, metaclass=ModelBoxBaseModelMetaclass):
+    Meta = None
+
     def __init__(self):
         self._signals = []
 
-        for attr in ModelBoxBaseModel.attrs.items():
+        for attr in type(self).attrs.items():
             sig_name: str = attr[0]
             sig_obj = attr[1]
             if isinstance(sig_obj, SignalBase):
@@ -47,31 +43,55 @@ class ModelBoxBaseModel(object, metaclass=ModelBoxBaseModelMetaclass):
                 self._signals.append(sig_obj)
 
     def to_dict(self):
-        meta_class = ModelBoxBaseModel.meta_dict
+        meta_dict = self.meta_dict
 
-        return {
-            "name": meta_class.get('name', self.__class__.__name__),
-            "signals": [sig.to_dict() for sig in self._signals]
+        res_dict = {
+            conf_name:
+                meta_dict.get(conf_name, None)
+            for conf_name in self.readable_conf() if meta_dict.get(conf_name, None) is not None
         }
+        res_dict['signals'] = [sig.to_dict() for sig in self._signals]
+
+        # 如果name为空，则使用类名
+        if meta_dict.get('name', None) is not None:
+            res_dict['name'] = type(self).__name__
+
+        return res_dict
+
+    # 哪些配置项可以读取
+    @staticmethod
+    def readable_conf():
+        return ["name", "position", "flag"]
+
+    @property
+    def meta_dict(self):
+        meta_class = type(self).Meta
+        if meta_class is not None:
+            return {
+                conf_name: getattr(meta_class, conf_name, None)
+                for conf_name in self.readable_conf()
+            }
 
 
-if __name__ == '__main__':
+class CONFIGURE(object):
 
-    class TestModel(ModelBoxBaseModel):
-        CLK1 = Wire("CLK")
-        RST = Wire("RST")
-        D = Wire("D", (0, 31))
-        Q = Reg("Q", (0, 31))
+    def __init__(self,
+                 node_pos_pair: Dict[Type[ModelBoxBaseModel], Tuple[int, int]],
+                 other_conf=None,
+                 colors: Tuple[str] = (
+                         "#ffa502", "#ff6348", "#ff4757", "#747d8c",
+                         "#2f3542", "#2ed573", "#1e90ff", "#3742fa",
+                         "#e84393", "#05c46b", "#ffd43b", "#ffa000"
+                 )):
+        if other_conf is None:
+            other_conf = dict()
+        self.POSITIONS_PAIR = node_pos_pair
+        self.COLORS = colors
+        self.OTHER_CONF = other_conf
 
-        class Meta:
-            name = "TestMode1l"
+        for node, pos in node_pos_pair.items():
+            node.Meta.position = pos
 
-
-    # a = (__import__('FrameworkNode'))
-    import inspect
-
-    for name, model in filter(
-            lambda x: inspect.isclass(x[1]) and issubclass(x[1], ModelBoxBaseModel) and not x[1] == ModelBoxBaseModel,
-            inspect.getmembers(sys.modules[__name__])):
-        # print(name, model)
-        print(model().to_dict())
+        for node, conf in other_conf.items():
+            for attr in conf.items():
+                setattr(node.Meta, attr[0], attr[1])
