1761a7058ed9b223c4784e53a73456970c08583d
W-Mai <1341398182@qq.com>
2022-04-29 20:05:52 +0800

    增加：描述信息

--- setup.py
+++ setup.py
@@ -22,15 +22,15 @@ python main.py [-h] --input INPUT --output OUTPUT [--fmt FMT]
 
 输出的图片效果如下
 <div style="background-color: white;">
-<img src="https://github.com/W-Mai/FrameworkDrawer/blob/master/imgs/schema.svg">
+<img src="https://raw.githubusercontent.com/W-Mai/FrameworkDrawer/63f0cfa34c3bb4190a72188e002fc8b9b2347516/imgs/schema.svg">
 </div>
 """
 
-
 setup(
     name="FrameworkDrawer",
-    version="0.0.4",
+    version="0.0.5",
     description="Draw a simple RTL block diagram.",
+    long_description_content_type="text/markdown",
     long_description=long_description,
     author="W-Mai",
     author_email="1341398182@qq.com",
